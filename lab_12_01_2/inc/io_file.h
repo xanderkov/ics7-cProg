1c602690dd9f15237367111156bda8aa8a084c1b
Kovel Alexander <xanderkovel@gmail.com>
2021-12-10 18:20:31 +0300

    Added dynamic library

---
+++ lab_12_01_2/inc/io_file.h
@@ -0,0 +1,9 @@
+#ifndef _READ_
+
+#define _READ_
+
+
+ARR_DLL int ARR_DECL read_file(char *file_name, int **arr, int *n);
+ARR_DLL void ARR_DECL print_array(char *file_name, int *arr, int *end);
+
+#endif
